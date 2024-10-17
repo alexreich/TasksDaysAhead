423088ab6ffa4f3ffb1d7f4e5b7a159c0fd92e39
areich <alex@alexreich.com>
2024-10-16 19:14:03 -0700

    cleaned up w/ai

--- TasksDaysAhead/Program.cs
+++ TasksDaysAhead/Program.cs
@@ -1,42 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Win32.TaskScheduler;
+﻿using Microsoft.Win32.TaskScheduler;
+using System;
+using System.Collections.Generic;
 using System.Configuration;
-using Task = Microsoft.Win32.TaskScheduler.Task;
+using System.Linq;
+using SchedulerTask = Microsoft.Win32.TaskScheduler.Task;
 
-EnumFolderTasks(TaskService.Instance.RootFolder);
-
-void EnumFolderTasks(TaskFolder fld)
+using (var taskService = new TaskService())
 {
-    List<Task> tasks = new List<Task>();
-    int Tasknum;
-    foreach (Task task in fld.Tasks)
-    {
-        var isNumeric = int.TryParse(task.Name, out int n);
-        if (isNumeric)
-        {
-            tasks.Add(task);
-        }
-    }
-    //today at 7am
-    var today = DateTime.Today;
-    today = today.AddHours(int.Parse(ConfigurationManager.AppSettings["startHour"])).AddMinutes(int.Parse(ConfigurationManager.AppSettings["startMinute"]));
+    EnumFolderTasks(taskService.RootFolder);
+}
 
-    int day = int.Parse(ConfigurationManager.AppSettings["daysahead"]);
-    tasks.OrderBy(o => int.Parse(o.Name)).ToList().ForEach(t =>
+void EnumFolderTasks(TaskFolder folder)
+{
+    int startHour = int.Parse(ConfigurationManager.AppSettings["startHour"]);
+    int startMinute = int.Parse(ConfigurationManager.AppSettings["startMinute"]);
+    int daysAhead = int.Parse(ConfigurationManager.AppSettings["daysahead"]);
+    string username = ConfigurationManager.AppSettings["username"];
+    string password = ConfigurationManager.AppSettings["password"];
+
+    DateTime startDateTime = DateTime.Today.AddHours(startHour).AddMinutes(startMinute);
+    List<SchedulerTask> tasks = folder.Tasks.Where(task => int.TryParse(task.Name, out _)).ToList();
+
+    foreach (var task in tasks.OrderBy(t => int.Parse(t.Name)))
     {
-        t.Enabled = true;
-        
-        Console.WriteLine(t.Name);
-        TaskDefinition td = t.Definition;
-        t.Definition.Triggers.First().StartBoundary = today.AddDays(day);
-        
-        Console.WriteLine(t.Definition.Triggers.First().StartBoundary);
-        day++;
+        UpdateTask(task, ref startDateTime, ref daysAhead, username, password);
+    }
+}
+
+void UpdateTask(SchedulerTask task, ref DateTime startDateTime, ref int dayOffset, string username, string password)
+{
+    try
+    {
+        task.Enabled = true;
+
+        TaskDefinition td = task.Definition;
+        Trigger trigger = td.Triggers.FirstOrDefault();
+        if (trigger != null)
+        {
+            trigger.StartBoundary = startDateTime.AddDays(dayOffset);
+        }
+
+        Console.WriteLine($"Task: {task.Name}, StartBoundary: {trigger?.StartBoundary}");
+
+        dayOffset++;
+
         td.Principal.RunLevel = TaskRunLevel.Highest;
         td.Principal.LogonType = TaskLogonType.InteractiveToken;
         td.Settings.MultipleInstances = TaskInstancesPolicy.IgnoreNew;
-        //td.Settings.RunOnlyIfLoggedOn = true;
-        TaskService.Instance.RootFolder.RegisterTaskDefinition(t.Name, t.Definition, TaskCreation.Update, ConfigurationManager.AppSettings["username"], ConfigurationManager.AppSettings["password"], TaskLogonType.InteractiveToken);
-    });
 
+        TaskService.Instance.RootFolder.RegisterTaskDefinition(task.Name, td, TaskCreation.Update, username, password, TaskLogonType.InteractiveToken);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Failed to update task '{task.Name}': {ex.Message}");
+    }
 }
